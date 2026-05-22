bce7f75c5fdaea907f32c09d59a20e86df22d1e2
ALVAROVELAME <alvarovelame@gmail.com>
2026-05-22 18:31:13 -0300

    fix: novo test de otimizacao

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,13 @@ export default defineConfig({
   plugins: [
     react(),
     viteImagemin({
+      // Configuração para forçar a compressão e otimização
+      // Nota: Alguns plugins de imagemin não mudam a extensão automaticamente no HTML,
+      // mas reduzem o peso do arquivo drasticamente.
       gifsicle: { optimizationLevel: 7, interlaced: false },
       optipng: { optimizationLevel: 7 },
       mozjpeg: { quality: 80 },
-      pngquant: { quality: [0.8, 0.9], speed: 4 },
+      pngquant: { quality: [0.7, 0.8], speed: 1 },
       svgo: {
         plugins: [{ name: 'removeViewBox' }, { name: 'removeEmptyAttrs', active: false }]
       }
